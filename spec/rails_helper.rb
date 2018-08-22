f01eb41830f79023442a437c3ddf3e52a32811f1
bghalami <benjaminghalami@gmail.com>
2018-08-22 17:49:09 -0600

    Add article model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,7 @@ end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     # Choose a test framework:
-      # with.test_framework :rspec
+    with.test_framework :rspec
     # Or, choose the following (which implies all of the above):
     with.library :rails
   end
